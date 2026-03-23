1ecec9533c98190cc46da34f8fbb5017299a0620
simonelusetti <simone.lusetti@unimore.it>
2026-03-23 15:24:25 +0100

    init content-only repo

---
+++ quartz.layout.ts
@@ -0,0 +1,59 @@
+import { PageLayout, SharedLayout } from "./quartz/cfg"
+import * as Component from "./quartz/components"
+
+// components shared across all pages
+export const sharedPageComponents: SharedLayout = {
+  head: Component.Head(),
+  header: [],
+  afterBody: [],
+  footer: Component.Footer({
+    links: {
+      GitHub: "https://github.com/jackyzha0/quartz",
+      "Discord Community": "https://discord.gg/cRFFHYye7t",
+    },
+  }),
+}
+
+// components for pages that display a single page (e.g. a single note)
+export const defaultContentPageLayout: PageLayout = {
+  beforeBody: [
+    Component.Flex({
+      components: [
+        {
+          Component: Component.Search(),
+          grow: true,
+        },
+        { Component: Component.Darkmode() },
+        { Component: Component.ReaderMode() },
+      ],
+    }),
+    Component.Explorer({ title: "" }),
+    Component.HorizontalDivider(),
+    Component.ArticleTitle(),
+    Component.TagList(),
+  ],
+  left: [],
+  right: [],
+}
+
+// components for pages that display lists of pages  (e.g. tags or folders)
+export const defaultListPageLayout: PageLayout = {
+  beforeBody: [
+    Component.Flex({
+      components: [
+        {
+          Component: Component.Search(),
+          grow: true,
+        },
+        { Component: Component.Darkmode() },
+        { Component: Component.ReaderMode() },
+      ],
+    }),
+    Component.Explorer({ title: "" }),
+    Component.HorizontalDivider(),
+    Component.ArticleTitle(),
+    Component.ContentMeta(),
+  ],
+  left: [],
+  right: [],
+}
